8b0d5a1402134a57260735ec9a50a6072a01b441
Lennart Chevallier <chevallier.lennart5@gmail.com>
2023-09-18 15:17:36 +0200

    updated documentation

--- R/candidates_maps.R
+++ R/candidates_maps.R
@@ -3,31 +3,32 @@
 #' The function applies the list of toponyms returned by \code{top.candidates()} to \code{top()}. A series of maps showing the toponym, ratio in percentage and numbers will be generated and locally saved.
 #'
 #'
-#' @param countries Character string with country code abbreviations (check \url{https://www.geonames.org/countries/} for a list of available countries) specifying, the toponyms of which countries are checked.
-#' @param count numeric. The number of the most frequent endings which will be tested, e.g. by default the top ten most frequent endings in Germany.
-#' @param len numeric. The character length of the endings, e.g. by default three-character-long endings.
+#' @param countries character string with country code abbreviations (check \url{https://www.geonames.org/countries/} for a list of available countries) specifying, the toponyms of which countries are checked.
+#' @param count numeric. The number of the most frequent endings which will be tested.
+#' @param len numeric. The character length of the endings.
 #' @param df logical. If \code{TRUE} then the filtered data frames will be saved in the global environment.
 #' @param csv logical. If \code{TRUE} then the filtered data frames will be saved as .csv in the current working directory.
 #' @param rat numeric. The ratio (a number between 0.0 and 1) of how many occurrences of one ending need to be in the polygon
-#' @param type character string. Either "$" (suffixes) or "^" (prefixes)
+#' @param type character string. Either by default "$" (ending) or "^" (beginning)
 #' @param lons numeric. Vector of longitudinal coordinates defining the polygon.
 #' @param lats numeric. Vector of latitudinal coordinates defining the polygon.
 #' @param feat.class character string with feature classes (check \url{http://download.geonames.org/export/dump/readme.txt} for the list and names of all feature classes in the data). By default, it is \code{p}.
 #' @details
 #' The goal is to find and generate maps of toponyms which are potentially worthwhile to be further examined by hand or other means.
-#' As a meaningful ratio is not determinable if we take the different areas (i.e. the possible polygons to be compared) and varying frequencies of specific toponyms into account.
+#' A general and meaningful ratio is not determinable if we take the different areas (i.e. the possible polygons to be compared) and varying frequencies of specific toponyms into account.
 #' For example, an ending may only occur five times in total, thus the distribution on a percentage basis is a less conclusive indicator for potential candidates.
 #'
 #'
-#' @return A number of data frames and plots saved in a sub folder (called "data frames" and "plots") in the working directory. It also stores the ratio surpassing toponyms in a data frame in the global environment.
+#' @return A number of data frames and plots saved in a sub folder (called "data frames" and "plots") in the working directory. If wanted, it stores the ratio surpassing toponyms in a data frame in the global environment.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' candidates.maps()
+#' candidates.maps(countries = "BE", count = 100, len = 3, rat = .5,
+#' lons = toponym::flanders_polygon$lons, lats = toponym::flanders_polygon$lats)
 #'
-#' ## generates and saves the data frames & maps of the top ten three-character-long endings
-#' ## in Germany if more than 50% of the places lie in the default polygon.
+#' ## generates and saves the data frames & maps of the top hundred three-character-long endings
+#' ## in Belgium if more than 50% of the places lie in the polygon for Flanders.
 #' }
 candidates.maps <- function(countries, count, len, df = FALSE, csv = TRUE, rat, type = "$", lons, lats, feat.class = "P")
   {
