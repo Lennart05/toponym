738cad888c0367c9d1c30e8303dec624ed97f360
Lennart Chevallier <chevallier.lennart5@gmail.com>
2023-09-18 15:22:55 +0200

    countryInfo documentation

---
+++ R/countryInfo.R
@@ -0,0 +1,6 @@
+#' @title country information
+#'
+#' @description
+#' A data frame which lists all countries available in the \code{geodata} package and GeoNames data set.
+#' The first column lists the ISO2 codes, the second the ISO2 codes and the third one all country names in full length.
+"countryInfo"
