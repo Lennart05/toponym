56b74e0669b96e0a269b53f68f44391a940dff9e
Lennart Chevallier <chevallier.lennart5@gmail.com>
2023-10-24 12:09:16 +0200

    updated documentation, imports moved here from other functions

--- R/toponym-package.R
+++ R/toponym-package.R
@@ -1,8 +1,36 @@
 #' @keywords internal
+#' @description
+#' A simple package for visualizing toponym distributions. You either provide a regular expression searching for places matching it or find the most frequent toponyms specific to one area.
+#'
+#' The main functions are the following:
+#' \itemize{
+#' \item\code{\link{top}} generates one map with all locations matching the regular expression
+#' \item\code{\link{country.data}} returns information on country references used by the package
+#' \item\code{\link{create.polygon}} lets users define a polygon by clicking on a map
+#' \item\code{\link{top.candidates}} generates a list of prefixes or suffixes frequent in one given region (as polygon)
+#' \item\code{\link{candidates.maps}} generates maps and lists on your computer out of these frequent prefixes and suffixes
+#' \item\code{\link{top.freq}} generates a list of the most frequent toponyms
+#' \item\code{\link{z.test}} lets users apply a z-test
+#' }
+#' For more detailed descriptions read the respective documentation.
+
 "_PACKAGE"
 
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom utils write.csv
+#' @importFrom utils download.file
+#' @importFrom utils unzip
+#' @importFrom utils read.table
+#' @importFrom geodata world
+#' @importFrom geodata gadm
+#' @importFrom grDevices chull
+#' @importFrom grDevices rainbow
+#' @importFrom dplyr %>%
+#' @importFrom dplyr mutate_at
+#' @importFrom sf st_as_sf
+#' @importFrom sp point.in.polygon
+#' @importFrom sp plot
+#' @importFrom spatstat.geom clickpoly
+#' @importFrom stats prop.test
 ## usethis namespace: end
 NULL
