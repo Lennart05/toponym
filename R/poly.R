86b41d52a6016df9b6f7d4cf55fb934556a280e9
Lennart Chevallier <chevallier.lennart5@gmail.com>
2023-10-24 12:10:05 +0200

    new function for polygon creation

---
+++ R/poly.R
@@ -0,0 +1,18 @@
+#' @title Transforms coordinates into a convex hull
+#'
+#' @param lons numeric. Vector of longitudinal coordinates defining the polygon.
+#' @param lats numeric. Vector of latitudinal coordinates defining the polygon.
+#'
+#' @return a convex hull
+#'
+poly <- function(lons, lats){
+
+  # store coordinates of the polygon in a df
+  pol  <- data.frame(X = lons, Y = lats)
+  # chull function from package grDevices
+  pos <- chull(pol)
+  con.hull <- rbind(pol[pos,],pol[pos[1],])   # convex hull
+
+  return(con.hull)
+
+}
