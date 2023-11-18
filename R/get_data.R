7408e3715744fa6b9d2a4839af1c60bbadce3013
Lenanrt Chevallier <chevallier.lennart5@gmail.com>
2023-11-18 16:37:00 +0100

    overwrite option and "all" addressing all stored txt files

--- R/get_data.R
+++ R/get_data.R
@@ -1,7 +1,8 @@
 #' @title Get toponym data from GeoNames
 #' @description The function downloads and saves toponym data from the [GeoNames download server](https://download.geonames.org/export/dump/), which is later used by \code{read.files()}.
-#' @param countries character string with country code abbreviations to download (check \url{https://www.geonames.org/countries/} for a list of available countries).
+#' @param countries character string with country code abbreviations to download (check \url{https://www.geonames.org/countries/} for a list of available countries). Type "all" for all currently stored data sets in the package directory.
 #' @param save logical. If \code{TRUE} then the data sets will be extracted to the package folder. It will be otherwise saved in the temporary folder.
+#' @param overwrite logical. If \code{TRUE} then the data sets (.txt files) in the package folder will be overwritten.
 #' @examples
 #' \dontrun{
 #' get.data(countries = c("DK", "DE"), save = FALSE)
@@ -13,35 +14,43 @@
 #' }
 #' @return The data as .txt in the temporary or package folder.
 #' @export
-get.data <- function(countries, save = TRUE) {
+get.data <- function(countries, save = TRUE, overwrite = FALSE) {
 
+  packdir <- system.file("extdata", package = "toponym")
+  if(countries == "all"){
+    countries <- substring(list.files(packdir), 1, 2)
+  }else{
+    for(i in 1:length(countries)){countries[i] <- country.data(query = countries[i])[,1]} #converts input into ISO2 codes
+    countries <- countries[!is.na(countries)] # removes incorrect country names
+  }
 
-  url <- paste0("https://download.geonames.org/export/dump/", countries, ".zip?raw=TRUE") # download address on GeoNames
   filename <- paste0(countries, ".txt")
   zipfile <- paste0(countries, ".zip")
   tmpdir <- tempdir()
   tmpfile <- paste0(tmpdir, "\\", zipfile)
   tmptxt <- paste0(tmpdir, "\\", countries, ".txt")
 
-  packdir <- system.file("extdata", package = "toponym")
-
+  url <- paste0("https://download.geonames.org/export/dump/", countries, ".zip?raw=TRUE") # download address on GeoNames
 
 
   for (i in 1:length(countries)) {
-    if(file.exists(paste0(packdir, "/", filename[i]))){
-  } else if(!file.exists(paste0(packdir, "/", filename[i])) && save == TRUE){  # checks if file exists in package directory
-
-        if(!file.exists(tmpfile[i])){utils::download.file(url[i],tmpfile[i], mode = "wb")}  # downloads zip if missing
-        utils::unzip(zipfile = tmpfile[i], files = filename[i], exdir = packdir, overwrite = FALSE) # unzips txt to package directory
-        print(paste(filename[i], "saved in package directory"))
-
-  } else if(!file.exists(tmptxt[i])){   # checks if file exists in tempdir
+    if(save == TRUE){
+    if(overwrite == FALSE){
+        if(!file.exists(paste0(packdir, "/", filename[i]))){ # checks if txt exists
+          if(!file.exists(tmpfile[i])){utils::download.file(url[i],tmpfile[i], mode = "wb")} # downloads zip if missing
+          utils::unzip(zipfile = tmpfile[i], files = filename[i], exdir = packdir, overwrite = FALSE) # unzips txt to package directory
+          print(paste(filename[i], "saved in package directory"))
+        }
+        }else{ # if overwrite is set to TRUE
+        if(!file.exists(tmpfile[i])){utils::download.file(url[i],tmpfile[i], mode = "wb")} # downloads zip if missing
+        utils::unzip(zipfile = tmpfile[i], files = filename[i], exdir = packdir, overwrite = TRUE) # unzips txt to package directory
+        print(paste(filename[i], "overwritten in package directory"))
+        }
+        }else if(!file.exists(tmptxt[i])){ # checks if txt exists in tempdir
         if(!file.exists(tmpfile[i])){utils::download.file(url[i],tmpfile[i], mode = "wb")} # downloads zip if missing
         utils::unzip(zipfile = tmpfile[i], files = filename[i], exdir = tmpdir, overwrite = FALSE) # unzip txt to tempdir
-  }
+        print(paste(filename[i], "saved in temporary directory"))
+      }
 
-    }
-
-
-
-}
+      }
+ }
