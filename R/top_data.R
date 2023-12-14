cb54af6e1dec3446795fa679a8aa1968e55bd419
Lennart Chevallier <chevallier.lennart5@gmail.com>
2023-12-14 15:33:24 +0100

    test function

--- R/top_data.R
+++ R/top_data.R
@@ -1,4 +1,6 @@
-topData <- function() {
-
-  return(package_env$de)
+topData <- function(countries) {
+  if(length(countries)>1){
+    warning("Multiple requests are not allowed. Only the first element will be used.")
+  }
+  return(top_env[[tolower(countries)]])
 }
