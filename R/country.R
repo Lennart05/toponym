5c0aa6dee0a377bfbd07ded57a5850f377d01b13
Lennart Chevallier <chevallier.lennart5@gmail.com>
2023-12-17 18:06:31 +0100

    multiple requests allowed, user warnings added

--- R/country.R
+++ R/country.R
@@ -23,34 +23,63 @@
 country <- function(query = NULL, regions = FALSE) {
   countryInfo <- toponym::countryInfo
 
-  if (length(query) > 1) {
-    stop("Multiple requests in a query are not allowed.")
-  }
-
+  output <- list()
+  warn <- list()
+  for(i in 1:length(query)){
   if (regions == FALSE) {
-    if (query == "country table") {
+    if (any(query == "country table")) {
       return(countryInfo)
-    } else if (query == "ISO2") { # outputs all ISO2 codes
+    } else if (query[i] == "ISO2") { # outputs all ISO2 codes
       return(countryInfo[, 1])
-    } else if (query == "ISO3") { # outputs all ISO3 codes
+    } else if (query[i] == "ISO3") { # outputs all ISO3 codes
       return(countryInfo[, 2])
-    } else if (query == "names") { # outputs all country names
+    } else if (query[i] == "names") { # outputs all country names
       return(countryInfo[, 3])
-    } else if (nchar(query) == 2) { # ISO2 code as input
-      output <- countryInfo[match(query, countryInfo[, 1]), ] # then outputs respective row
-    } else if (nchar(query) == 3) { # ISO3 code as input
-      output <- countryInfo[match(query, countryInfo[, 2]), ] # then outputs respective row
-    } else if (nchar(query) >= 4) { # country name as input
-      output <- countryInfo[match(query, countryInfo[, 3]), ] # then outputs respective row
+    } else if (nchar(query[i]) == 2) { # ISO2 code as input
+      query[i] <- toupper(query[i])
+      output[[i]] <- countryInfo[match(query[i], countryInfo[, 1]), ] # then outputs respective row
+    } else if (nchar(query[i]) == 3) { # ISO3 code as input
+      query[i] <- toupper(query[i])
+      output[[i]] <- countryInfo[match(query[i], countryInfo[, 2]), ] # then outputs respective row
+    } else if (nchar(query[i]) >= 4) { # country name as input --> there is no country name shorter than 4 characters
+      output[[i]] <- countryInfo[match(query[i], countryInfo[, 3]), ] # then outputs respective row
     }
-    if (is.na(output[1])) {
-      stop("The query contains no valid country reference")
+    if (anyNA(output[[i]])) {
+      warn[[i]] <- query[i]
     } # check for NAs
   } else { # if regions is TRUE
 
     map_path <- paste0(system.file(package = "geodata"), "/extdata")
-    output <- gadm(country = query, path = map_path)$NAME_1
-    Encoding(output) <- "UTF-8" # corrects encoding
+
+    error <- paste0("The query '", query[i], "' is an invalid country reference")
+
+    output[[i]] <- tryCatch(expr = {
+      gadm(country = query[i], path = map_path)$NAME_1
+    },
+    error = function(e){
+      warning(error)
+      return(NA)
+    }
+    )
+    if(!all(is.na(output[[i]]))){
+      Encoding(output[[i]]) <- "UTF-8" # corrects encoding
+    }
   }
+
+  } # loop ends
+
+  n_warn <- 0
+  if(length(warn) > 0){
+    n_warn <- sum(sapply(warn, is.character))
+    warn <- warn[!sapply(warn, is.null)]
+    }
+  if(n_warn == 1){
+    warning(paste0("The query '", warn, "' is an invalid country reference."))
+  }else if(n_warn > 1){
+    warning(paste0("The queries '", paste(warn, collapse = ", "), "' are invalid country references."))
+  }
+  if(n_warn == length(query))
+    stop("There were no correct inputs. No data returned.")
+  output <- output[!sapply(output, function(x) all(is.na(x)))]
   return(output)
 }
