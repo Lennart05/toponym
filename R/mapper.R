728318ec723b353ffbbcba78064344938ba59c26
Lennart Chevallier <chevallier.lennart5@gmail.com>
2024-01-09 09:31:29 +0100

    test

---
+++ R/mapper.R
@@ -0,0 +1,12 @@
+mapper <- function(mapdata, ...){
+
+coordinates <- list()
+
+
+
+
+
+simpleMap(strings, coordinates, opt$color, opt$regions, opt$plot, opt$polygon, opt$ratio_string, opt$fq)
+
+
+}
