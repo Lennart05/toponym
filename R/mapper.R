9fb3f40699dec38c762a9877163137dbd6edbfa4
Lennart Chevallier <chevallier.lennart5@gmail.com>
2024-01-12 15:29:47 +0100

    lets users enter own data

--- R/mapper.R
+++ R/mapper.R
@@ -1,24 +1,43 @@
+#' @title Plots data onto a map.
+#' @description
+#' This function plots a user-specific data frame onto a map.
+#'
+#' @param mapdata data frame. A user-specific data frame with coordinates.
+#' @param ... Additional parameters:
+#' \itemize{
+#' \item\code{color} character string indicating, which color is assigned to each string.
+#' \item\code{regions} numeric. Specifies the level of administrative borders. By default \code{0} displaying only country borders.
+#' \item\code{plot} logical. If \code{FALSE}, the plot will not be printed but saved as .png in the current working directory.
+#' \item\code{label} character string. Text for the title of plot.
+#' }
+#' @details
+#' This function's purpose is to allow users to provide own data frames or curated ones exported by this package.
+#' The data frame must have at least two columns called `latitude` & `longtitude`. The optional parameter \code{color} is ignored, if the data frame contains a column `color`.
+#' If `regions`  is set to a value higher than 0, either the data frame must have a column `country codes` or the parameter \code{countries} must be defined.
+#' Parameter \code{countries} accepts all references found in \code{country(query = "country table")}.
+#' @return A plot.
+#' @export
+#'
 mapper <- function(mapdata, ...){
-
-if(!is.data.frame(mapdata)) stop("'mapdata' must be a data frame.")
-if(!all(c("name", "latitude", "longitude") %in% colnames(mapdata))) stop("'mapdata' must have the following columns: 'name', 'latitude' & 'longitude'.")
-#if("color" %in% colnames(mapdata))
-#if(!"country code" %in% colnames(mapdata)) warning("'mapdata does not contain ")
-
-
-
-#sum(is.na(mapdata$`country code`))
-
-
-coordinates <- list(latitude = mapdata$`latitude`, longitude = mapdata$`longitude`, "country code" = mapdata$`country code`, matches = mapdata$`matches`, color = mapdata$`color`)
 
 opt <- list(...)
 if (is.null(opt$regions)) opt$regions <- 0
 if (is.null(opt$plot)) opt$plot <- TRUE
 
+if(!is.data.frame(mapdata)) stop("'mapdata' must be a data frame.")
+if(!all(c("name", "latitude", "longitude") %in% colnames(mapdata))) stop("'mapdata' must have the following columns: `latitude' & 'longitude'.")
+if(!"country code" %in% colnames(mapdata) && opt$regions > 0) stop("Since no country codes were provided, parameter `regions` cannot exceed 0.")
+
+if("color" %in% colnames(mapdata)) warning(paste("sum(is.na(mapdata$`color`) entries are empty in the color column."))
+
+
+
+coordinates <- list(latitude = mapdata$`latitude`, longitude = mapdata$`longitude`, "country code" = mapdata$`country code`, matches = mapdata$`matches`, color = mapdata$`color`)
+
+
 
 simpleMap(opt$label, #optional labels
-          coordinates, opt$color, opt$regions, opt$plot, opt$polygon)
+          coordinates, opt$color, opt$regions, opt$plot)
 
 
 }
