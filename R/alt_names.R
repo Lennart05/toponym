e106f0e445831a5314952bd3cedca891e136a7fc
Lennart Chevallier <chevallier.lennart5@gmail.com>
2024-01-12 15:21:47 +0100

    moved list, added error message

--- R/alt_names.R
+++ R/alt_names.R
@@ -11,16 +11,17 @@ altNames <- function(gn, strings) {
   alt_names <- strsplit(gn$alternatenames, ",") # separate names in altnam col
   alt_names <- do.call(rbind, unname(lapply(alt_names, `length<-`, max(lengths(alt_names))))) # as matrix
   alt_l <- list()
+  m_strings <- list()
 
   for (i in 1:nrow(alt_names)) {
     alt_l[[i]] <- grepl(paste(strings, collapse = "|"), alt_names[i, ], perl = TRUE) # check all alt names for reg ex match
     alt_l[[i]] <- any(alt_l[[i]])
   }
+  if(!any(unlist(alt_l))) stop("\nThere were no matches.\n")
 
   w_strings <- as.logical(unlist(alt_l))
   alt_names <- alt_names[w_strings, ]
 
-  m_strings <- list()
   for (i in 1:nrow(alt_names)) {
     m_strings[[i]] <- regmatches(alt_names[i, ], regexpr(paste(strings, collapse = "|"), alt_names[i, ], perl = TRUE)) # gets matches
   }
