8c94995d7b95e351382a3368bf4762b23a24b3b8
Lennart Chevallier <chevallier.lennart5@gmail.com>
2024-01-18 10:56:36 +0100

    data reversed only if necessary

--- R/poly.R
+++ R/poly.R
@@ -1,7 +1,7 @@
 #' @title Transforms coordinates into a window
 #' @description
 #' This function transforms polygonal data into an object of class `owin` for \code{spatstat.geom} functions.
-#'
+#' If data points run clockwise, data will be reversed for \code{owin} function. It requires counterclockwise data.
 #' @param polygon data frame, coordinates of the polygon.
 #' @keywords internal
 #'
@@ -10,8 +10,18 @@
 poly <- function(polygon) {
   pol <- list(x = polygon$lons, y = polygon$lats)
 
+  x.coords <- c(pol$x, pol$x[1])
+  y.coords <- c(pol$y, pol$y[1])
+
+  double.area <- sum(sapply(2:length(x.coords), function(i) {
+    (x.coords[i] - x.coords[i-1])*(y.coords[i] + y.coords[i-1])
+  }))
+
+  if(double.area > 0) pol = lapply(pol, rev) # check if points run clockwise. If so, reverse it
+  #owin function require polygon data to be COUNTER clockwise
+
   #owin function from spatstat.geom
-  poly_owin <- owin(poly = lapply(pol, rev))
+  poly_owin <- owin(poly = pol)
 
   return(poly_owin)
 }
