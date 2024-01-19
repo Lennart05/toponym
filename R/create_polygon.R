64636493cf251e467d3785e3870c7ba74dfb8ab3
Lennart Chevallier <chevallier.lennart5@gmail.com>
2024-01-19 10:49:34 +0100

    accepts region ID and name. updated docu

--- R/create_polygon.R
+++ R/create_polygon.R
@@ -6,15 +6,19 @@
 #' \itemize{
 #' \item\code{regions} numeric. Specifies the level of administrative borders. By default \code{0} for displaying only country borders.
 #' \item\code{region_name} character string with region name.
+#' \item\code{region_ID} character string with region ID.
 #' \item\code{retrieve} logical. If \code{TRUE}, the coordinates of the region or country are returned. No map will be drawn.
 #' }
 #' @export
 #' @details
 #' Parameter \code{countries} accepts all designations found in \code{country(query = "country table")}.
-#' \code{region_name} accepts region names for the selected countries, which can also be retrieved by \code{country()}.
-#' This function uses the function \code{spatstatLocator} provided by the \code{spatstat.utils} package. The maps are retrieved from the \code{geodata} package.
+#' \code{region_ID} and \code{region_name} accepts region designations for the selected countries, which can be retrieved by \code{country()}.
+#' The function prioritizes any \code{region_ID} and ignores \code{region_name} if users provide both.
+#' The matrix listing all region designations may be incomplete as the \code{geodata} mapa data is incomplete in this regard. For mapping purposes, \code{geodata} is used throughout this package.
 #'
-#' In RGui, users either have to middle-click or right-click and then press stop.
+#'
+#' This function uses the function \code{spatstatLocator} provided by the \code{spatstat.utils} package for the point-and-click functionality.
+#' In RGui, users exit the point selection by middle-clicking or right-clicking and then pressing stop.
 #'
 #' In RStudio, users exit the point selection by pressing ESC or Finish in the top right corner of the plot.
 #' Users whose points are shifted away, are advised to set the zoom settings of RStudio and of their device to 100%:
@@ -22,17 +26,17 @@
 #' Tools -> Global Options -> Appearance -> Zoom
 #'
 #'
-#' For further details on the point-and-click mechanisms refer to the help page for \code{spatstatLocator}.
+#' For further details on the point-and-click mechanism, please refer to the help page for \code{spatstatLocator}.
 #'
 #' @examples
 #' \dontrun{
-#' createPolygon("NA", region_name = "Ohangwena")
+#' createPolygon("NA", region_ID = "NAM.7_1")
 #'
 #' # a plot of the region Ohangwena in Namibia
 #' # by point-and-click a polygon can be created
-#'
+#' # use country() to find all acceptable region IDs
 #' Ohangwena_polygon <- createPolygon(
-#' "NA", region_name = "Ohangwena", retrieve = TRUE
+#' "NA", region_ID = "NAM.7_1", retrieve = TRUE
 #' )
 #' # no plot appears
 #' # the coordinates of the region are stored in the object
@@ -47,20 +51,30 @@ createPolygon <- function(countries, ...) {
   opt <- list(...)
   if(is.null(opt$regions)) opt$regions <- 0
   if(is.null(opt$retrieve)) opt$retrieve <- FALSE
-  if(is.logical(opt$regions)) stop("Parameter `regions` must be numeric, not logical.")
+  if(!is.numeric(opt$regions)) stop("Parameter `regions` must be numeric.")
 
   if (any(countries == "world")) {
     countries <- "world"
     map <- world(path = map_path) # world map
-  } else if (is.null(opt$region_name)) { # if no region provided
+  } else if (all(is.null(opt$region_ID), is.null(opt$region_name))) { # if no region provided
     map <- gadm(country = countries, level = opt$regions, path = map_path) # country map
-  } else { # if region name is provided
-    if (opt$regions == 0) {
-      opt$regions <- 1
-    } # admin level = regions needs to be at least 1 if specific regions are to be displayed
-    map <- gadm(country = countries, level = opt$regions, path = map_path)
-    Encoding(map$NAME_1) <- "UTF-8"
-    map <- map[map$NAME_1 %in% opt$region_name, ]
+
+
+
+
+  } else if(!all(is.null(opt$region_ID), is.null(opt$region_name))) { # if region designation is provided
+    if (opt$regions == 0) opt$regions <- 1 # admin level = regions needs to be at least 1 if specific regions are to be displayed
+
+    map <- gadm(country = countries, level = opt$regions, path = map_path)  ## country map first
+    if(is.null(map)) stop(paste("Map data could not be retrieved.", if(opt$regions >= 1) "'regions' parameter may be set too high"))
+
+    if(!is.null(opt$region_ID)){ # FIRST region ID
+    map <- map[map$GID_1 %in% opt$region_ID]   ## selection of region within country
+    }else if(!is.null(opt$region_name)){ # ELSE region name
+    map <- map[map$NAME_1 %in% opt$region_name]   ## selection of region within country
+    }
+
+    if(length(map) == 0) stop("Map data could not be retrieved. Region designation may be invalid.")
   }
 
   if (opt$retrieve == TRUE) { # if polygon data is to be extracted
@@ -70,6 +84,7 @@ createPolygon <- function(countries, ...) {
       stop("'world' is not a valid query for polygon retrival.")
     }
     polygon <- as.data.frame(crds(map)) # retrieves coordinates of subset or country from map data
+
   } else { # lets users draw on subset or country
     terra::plot(map) # plots the map
     polygon <- spatstatLocator(type = "o") # lets users draw a polygon on the plotted map
