44f10cf7a77f6d19ddb365cfe52f9d5f1f5dece3
Lennart Chevallier <chevallier.lennart5@gmail.com>
2024-01-21 13:07:02 +0100

    line breaks in documentation

--- R/mapper.R
+++ R/mapper.R
@@ -13,10 +13,15 @@
 #' }
 #' @details
 #' This function's purpose is to allow users to provide own data frames or curated ones exported by this package.
+#'
 #' The data frame must have at least two columns called `latitude` & `longtitude`.
+#'
 #' If the data frame has a column `color`, the function will assign every value in that column to the respective coordinates and ignore the additional parameter \code{color}.
+#'
 #' If the data frame has a column `group`, the function will group data and display a legend.
+#'
 #' If the data frame has a `color` and a `group` column, the assignment must match each other. Every `group` must be assigned a unique color throughout the data frame.
+#'
 #' If `regions`  is set to a value higher than 0, the data frame must have a column `country codes`.
 #' @return A plot.
 #' @export
@@ -31,6 +36,7 @@ if(!is.data.frame(mapdata)) stop("Parameter 'mapdata' must be a data frame.")
 if(!all(c("latitude", "longitude") %in% colnames(mapdata))) stop("Parameter `mapdata` must have the following columns: `latitude` & `longitude`.")
 if(!any(is.numeric(c(mapdata$latitude, mapdata$longitude)))) stop("The columns  `latitude` & `longitude` must be numeric.")
 if(!"country code" %in% colnames(mapdata) && opt$regions > 0) stop("Since no country codes were provided, parameter `regions` cannot exceed 0.")
+if(!is.null(mapdata$group) && is.logical(mapdata$group)) stop("The column `group` cannot be logical.")
 
 if(sum(is.na(mapdata$`color`)) > 0){
 if("color" %in% colnames(mapdata)) warning(paste(sum(is.na(mapdata$`color`))), " entries are empty in the color column.")
@@ -45,7 +51,6 @@ if(!is.null(mapdata$color) &&  !is.null(mapdata$group)){
 
 
 coordinates <- list(latitude = mapdata$`latitude`, longitude = mapdata$`longitude`, "country code" = mapdata$`country code`, group = mapdata$`group`, color = mapdata$`color`)
-
 
 
 
