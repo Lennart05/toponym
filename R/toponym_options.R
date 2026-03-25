ac1e63feb6d72f1af76e7a048194d108e236fad9
Lennart <mail@lchevallier.de>
2026-03-25 15:37:52 +0100

    no default setting, instead path must be specified

--- R/toponym_options.R
+++ R/toponym_options.R
@@ -1,33 +1,49 @@
 #' @title Manage Options of \code{toponym}
-#' @description This function allows users to modify a setting for managing toponym data.
-#' Users can specify whether toponym data retrieved from `GeoNames` will be saved in the package folder or in a temporary folder.
+#' @description 
+#' This function allows users to set the download path for the `toponym` package.
+#' Most functions require external data which will be downloaded and stored for later use. This is described in the respective functions.
+#' For this reason, after installation, users will be asked to specify the path for downloaded data. 
+#' Users can set the path to the package directory with this command:
+#' 
+#' \code{toponymOptions(toponym_path = "pkgdir")}
+#' 
+#' A full, alternative path can also be provided.
+#' 
+#' 
+#' If a path is provided, users are asked to confirm their choice. This function will write the new path into `toponym_options.rds`in the package directory; the path is saved across sessions. 
+#' To locate `toponym_options.rds`, enter:
+#' 
+#' \code{system.file("extdata", package = "toponym")}
 #'
+#'
+#' To check the path that is currently set, enter:
+#' 
+#' `toponymOptions()`
 #' @details
-#' Parameter `toponym_path`: if the current setting is `TRUE`, toponym data sets will be saved in the package folder; 
-#' if the current setting is `FALSE`, toponym data sets will be saved in a temporary folder.
+#' Parameter `toponym_path` accepts either the character string `"pkgdir"` or full, alternative paths.
+#' `pkgdir` is interpreted as the extdata folder in the `toponym` package directory, i.e.:
+#' \code{system.file("extdata", package = "toponym")}
+#' @param toponym_path character string. Path name for downloaded data. This setting is saved across sessions.
 #' 
-#' If no parameter is set, i.e. `toponymOptions()`, the complete data frame with current settings is printed.
-#' 
-#' @param toponym_path logical. Enter `TRUE` or `FALSE`. Allows the user to modify the setting for saving toponym data sets in the package folder or in a temporary folder.
-#' 
-#' @return A character string.
+#' @return A character string indicating the current path for downloaded data.
 #' 
 #' @examples
-#' \dontrun{
-#' # Show the current path
+#' if(interactive()){
+#' #Set the path to the temporary directory
+#' toponymOptions(toponym_path = tempdir())
+#' #Show the current path
 #' toponymOptions()
 #' }
-#' 
 #' @export
 toponymOptions <- function(toponym_path = NULL) {
-  #read path
+  #read current path
   toponym_options <- readRDS(paste0(system.file("extdata", package = "toponym"), "/toponym_options.rds"))
   
-  #check if path contains valid dir
-  if(!dir.exists(toponym_options)) stop("No valid path for downloaded data specified. Write toponymOptions(toponym_path = \"pkgdir\") to set the path to the package directory or provide a full, alternative path.")
+  if(is.character(toponym_path)) toponym_options <- save_path(toponym_path, toponym_options)
+
+  #check if path is NULL and if not, if it is a valid dir
+  if(is.null(toponym_options)) stop("\nNo valid path for downloaded data specified.\nWrite toponymOptions(toponym_path = \"pkgdir\") to set the path to the package directory or provide a full, alternative path.")
+  if(!dir.exists(toponym_options)) stop("\nNo valid path for downloaded data specified.\nWrite toponymOptions(toponym_path = \"pkgdir\") to set the path to the package directory or provide a full, alternative path.")
   
-  #sets path
-  toponym_options <- as.character(toponym_path) #set path if specified by user
-  saveRDS(toponym_options, paste0(system.file("extdata", package = "toponym"), "/toponym_options.rds"))
   return(toponym_options)
 }
