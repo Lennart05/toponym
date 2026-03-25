5be9e3bcc12eeae0240bf6a65e92817ba6e9ecfa
Lennart <mail@lchevallier.de>
2026-03-25 15:30:03 +0100

    updated doc & examples, returns used path

--- R/get_data.R
+++ R/get_data.R
@@ -3,31 +3,45 @@
 #' @details
 #' The data is downloaded from the [GeoNames download page](https://download.geonames.org/export/dump/) and thereby made accessible to \code{readFiles()}. The function allows users to update GeoNames data and to set the date of access to that database to the current date.
 #' Parameter \code{countries} accepts all designations found in \code{country(query = "country table")}. If "all", data from all countries stored in the package folder is selected.
-#' With \code{toponymOptions()}, users can specify whether toponym data downloaded by \code{getData()} will be stored in the package folder or in a temporary folder. See `help(toponymOptions)`.
+#' Parameter \code{toponym_path} accepts "pkgdir" for the package directory or a full, alternative path.
+#' With \code{toponymOptions()}, users can specify the path for toponym data downloaded by \code{getData()} across sessions. See `help(toponymOptions)`.
 #' @param countries character string vector with country designations (names or ISO-codes).
 #' @param overwrite logical. If \code{TRUE}, the data sets (.txt files) in the package folder will be overwritten.
+#' @param toponym_path character string. Path name for downloaded data. If not specified, this function will call `toponymOptions()` and try use the persistent path.
 #' @seealso [GeoNames download page](https://download.geonames.org/export/dump/)
 #' @examples
-#' \dontrun{
-#' getData(countries = "NL")
-#' ## downloads and extracts data for NL to the package folder (default setting)
+#' ## We recommend setting a persistent path for downloaded data by using toponymOptions()
+#' ## Users can always set the path manually when a function is used
+#' ## For illustration purposes, the path is manually set each time in the following examples:
+#' \donttest{
+#' getData(countries = "NL", toponym_path = tempdir())
+#' ## downloads and extracts data for NL to the temporary directory
 #' 
-#' getData(countries = c("DK", "DE"))
-#' ## downloads and extracts data for DK and DE to the package folder (default setting)
+#' getData(countries = c("DK", "DE"), toponym_path = tempdir())
+#' ## downloads and extracts data for DK and DE to the temporary directory
 #'
-#' getData(countries = c("DK", "DE"), overwrite = TRUE)
-#' ## downloads, extracts, and overwrites data for DK and DE in the package folder (default setting)
+#' getData(countries = c("DK", "DE"), overwrite = TRUE, toponym_path = tempdir())
+#' ## downloads, extracts, and overwrites data for DK and DE in the temporary directory
 #' }
+#' @return Character string of the used path for downloaded data.
 #' @export
-getData <- function(countries, overwrite = FALSE) {
-  toponym_options <- toponymOptions()
+getData <- function(countries, overwrite = FALSE, toponym_path = NULL) {
+  #toponym is NULL, read .Rds for path
+  if(is.null(toponym_path)){
+  toponym_path <- toponymOptions()
+  }else{
+  #use pkg dir if requested
+  if(toponym_path == "pkgdir") toponym_path <- system.file("extdata", package = "toponym")
+  #check if path is a valid dir
+  if(!dir.exists(toponym_path)) stop("\nNo valid path for downloaded data specified.\nWrite toponymOptions(toponym_path = \"pkgdir\") to set the path to the package directory or provide a full, alternative path.")
+  }
   
 	countries <- unlist(lapply(country(query = countries), function(x) x[, 1])) # convert if necessary designations to ISO2
 
   file_txt <- paste0(countries, ".txt")
-  file_txt_dir <- paste0(toponym_options, "/", file_txt)
+  file_txt_dir <- paste0(toponym_path, "/", file_txt)
   file_zip <- paste0(countries, ".zip")
-  file_zip_dir <- paste0(toponym_options, "/", file_zip)
+  file_zip_dir <- paste0(toponym_path, "/", file_zip)
   
   url <- paste0("https://download.geonames.org/export/dump/", countries, ".zip?raw=TRUE") # download address on GeoNames
 
@@ -37,12 +51,15 @@ getData <- function(countries, overwrite = FALSE) {
         if (any(!file.exists(file_txt_dir[i]), overwrite)) {#download if file is missing or overwrite = TRUE
           if (!file.exists(file_zip_dir[i])) {#zipfile is missing in directory
             utils::download.file(url[i], file_zip_dir[i], mode = "wb")
+            message(paste(file_zip[i], "saved in:", toponym_path))
           } # downloads zip if missing
-          utils::unzip(zipfile = file_zip_dir[i], files = file_txt[i], exdir = toponym_options, overwrite = overwrite) # unzips txt to directory & overwrite if set TRUE
-          if(overwrite == FALSE) {message(paste(filename[i], "saved in:", toponym_options))
-          }else message(paste(filename[i], "overwritten in:", toponym_options))
+          utils::unzip(zipfile = file_zip_dir[i], files = file_txt[i], exdir = toponym_path, overwrite = overwrite) # unzips txt to directory & overwrite if set TRUE
+          if(overwrite == FALSE) {message(paste(file_txt[i], "saved in:", toponym_path))
+          }else message(paste(file_txt[i], "overwritten in:", toponym_path))
         }
   }
+  
+  return(invisible(toponym_path))
   
   
 }
